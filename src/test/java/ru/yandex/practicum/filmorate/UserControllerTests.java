9400f51e019f46088a60a6525c552a172272a6f0
9138521930pv-prog <9138521930.pv@gmail.com>
2026-01-18 16:24:46 +0700

    Fix style error

--- src/test/java/ru/yandex/practicum/filmorate/UserControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/UserControllerTests.java
@@ -7,9 +7,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.controller.UserController;
-import ru.yandex.practicum.filmorate.model.User;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
