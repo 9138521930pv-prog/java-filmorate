43a078f8da8df3db9bdf228c8a2ce5b806f80860
9138521930pv-prog <9138521930.pv@gmail.com>
2026-02-23 14:50:37 +0700

    fix стиля

--- src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmControllerTests.java
@@ -1,20 +1,20 @@
 package ru.yandex.practicum.filmorate;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.yandex.practicum.filmorate.controller.FilmController;
 import ru.yandex.practicum.filmorate.storage.film.FilmStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserStorage;
-
+/*
+import org.springframework.http.MediaType;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
+*/
 @SpringBootTest
 @AutoConfigureMockMvc
 public class FilmControllerTests {
